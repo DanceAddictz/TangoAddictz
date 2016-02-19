7fbf0ea21a9f222c603a369991ed338beb74fb64
GIT Admin <sm.zeal@gmail.com>
2016-02-19 10:12:52 +0530

    Minor Changes

--- app/src/main/java/com/danceaddictz/tangoaddictz/view/HomeActivity.java
+++ app/src/main/java/com/danceaddictz/tangoaddictz/view/HomeActivity.java
@@ -1,17 +1,24 @@
 package com.danceaddictz.tangoaddictz.view;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.danceaddictz.tangoaddictz.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     public static Intent makeIntent(Context context){
 
@@ -29,7 +36,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+         toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle("TangoAddictz");
+        toolbar.setSubtitle("Welcome, TangoAddict");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -40,6 +51,25 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
